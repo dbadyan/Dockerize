bc17470b41788b2df1aea650fbc3c93c477c7b13
Daniel Badyan <dbadyan@gmail.com>
2021-04-15 22:10:21 +0300

    connected html and flask

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Dockerize it!'
+def index():
+    return render_template('index.html')
+
+
+@app.route("/generate/", methods=['POST'])
+def generate():
+    source = request.form.get('source')
+    generate_message = "FROM " + source
+    return render_template('index.html', generate=generate_message)
