06f0d6c9135444a19333faf1fdfabc025bf014a0
Daniel Badyan <dbadyan@gmail.com>
2021-04-29 17:47:40 +0300

    added select2 non operational

--- app.py
+++ app.py
@@ -1,16 +1,29 @@
-import logging
-import sys
-
-import docker
+import docker, requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
-app.logger.addHandler(logging.StreamHandler(sys.stdout))
-app.logger.setLevel(logging.ERROR)
 
 
 @app.route('/')
 def index():
+    url = "https://hub.docker.com/api/content/v1/products/search?page_size=25&q=hylang&type=image&image_filter=official"
+    page = requests.get(url)
+    image_json = page.json()['summaries']
+    # print(image_json)
+    print("#################################")
+
+    def search(images_original):
+        return [element for element in images_original]
+
+    result = search(image_json)
+    images = ""
+    for image in result:
+        images += " OR " + image['name']
+    if result is not None:
+        get_images_message = images
+        print(get_images_message)
+    else:
+        print("no source image")
     return render_template('index.html')
 
 
@@ -37,7 +50,16 @@ def get_images():
     return render_template('index.html', getImages="choose from the following images: " + get_images_message)
 
 
+@app.route("/scrape_for_images/", methods=['POST'])
+def scrape_for_images():
+    url = "https://hub.docker.com/search?q=ubuntu&type=image"
+    page = requests.get(url)
+    print(page.content)
+
+
 @app.route("/generate/", methods=['POST'])
 def generate():
-    chosen_image = request.form.get('chosen')
-    return render_template('index.html', generatedMessage="FROM " + chosen_image)
+    # chosen_image = request.form['chosen']
+    req = request.form['chosen']
+
+    return render_template('index.html', generatedMessage="FROM " + req)
